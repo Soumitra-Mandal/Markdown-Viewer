f9b15c6e06ea0e3f491337af45ad15e908c6c5ca
Soumitra Mandal <soumitramandal1999@gmail.com>
2021-05-28 10:37:45 +0530

    react markdown done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import ReactMarkdown from 'react-markdown'
+
 
 function App() {
+
+const [txt,setTxt] = useState('');
+
+const handleChange = (e) => {
+ //var markdown = marked(e.target.value);
+ setTxt(e.target.value);
+ 
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='editor' className='column'>
+      <h1 className='header'>Markdown</h1>
+    <textarea rows="40" cols="90" value={txt} onChange={handleChange}></textarea>
+    </div>
+    <div id='display' className='column'>
+      <h1 className='header'>Display</h1>
+      <div id='disp'>
+      <ReactMarkdown>{txt}</ReactMarkdown>
+      </div>
+    </div>
     </div>
   );
 }
